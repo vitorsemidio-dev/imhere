f6e2983a2978651f12fcf6e9110b65c441ec43ca
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-12-19 19:43:10 -0300

    :lipstick: add event name and event date in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#131016',
+        padding: 24,
+      }}
+    >
+      <Text
+        style={{
+          color: '#fff',
+          fontWeight: 'bold',
+          fontSize: 24,
+          marginTop: 48,
+        }}
+      >
+        Evento de React Native
+      </Text>
+      <Text
+        style={{
+          color: '#aaaaaa',
+        }}
+      >
+        Segunda, 19 de dezembro de 2022
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
