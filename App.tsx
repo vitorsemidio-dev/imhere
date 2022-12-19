dddb065d1abe1040c70fac0c723390f1d646a7db
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-12-19 19:47:26 -0300

    :recycle: style elements with StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,31 +1,27 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#131016',
-        padding: 24,
-      }}
-    >
-      <Text
-        style={{
-          color: '#fff',
-          fontWeight: 'bold',
-          fontSize: 24,
-          marginTop: 48,
-        }}
-      >
-        Evento de React Native
-      </Text>
-      <Text
-        style={{
-          color: '#aaaaaa',
-        }}
-      >
-        Segunda, 19 de dezembro de 2022
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Evento de React Native</Text>
+      <Text style={styles.eventDate}>Segunda, 19 de dezembro de 2022</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24,
+  },
+  eventName: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginTop: 48,
+  },
+  eventDate: {
+    color: '#aaaaaa',
+  },
+});
